065741e9adc49d7e6c12c46d4fb6056927bb2bb2
xieisabug <136419808@qq.com>
2024-07-25 22:36:05 +0800

    增加一些提示

--- src/components/AskWindowPrepare.tsx
+++ src/components/AskWindowPrepare.tsx
@@ -3,6 +3,20 @@ const AskWindowPrepare: React.FC = () => {
     <p>输入文本后回车，与快捷对话助手进行交流</p>
     <p>拖拽或者粘贴文件/图片后，可与快捷对话助手根据文件进行交流</p>
     <p>对话中可以使用以下!bang命令：</p>
+    <div>
+        <div>
+            <span>!s</span>
+            <span>插入选择的文字</span>
+        </div>
+        <div>
+            <span>!cd</span>
+            <span>插入当前日志文本：</span>
+        </div>
+        <div>
+            <span>!ct</span>
+            <span>插入当前时间文字：</span>
+        </div>
+    </div>
 </div>
 }
 
