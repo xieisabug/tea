e275d86d2b83b39b4c394e9afa14e59e9da7fb48
xieisabug <136419808@qq.com>
2024-10-01 22:53:39 +0800

    加载插件文件

--- src/PluginWindow.tsx
+++ src/PluginWindow.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { appDataDir } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
 
 function PluginWindow() {
     const [pluginNode, setPluginNode] = useState<React.ReactNode>(null);
@@ -7,30 +8,37 @@ function PluginWindow() {
     useEffect(() => {
         const initPage = async () => {
             const dirPath = await appDataDir();
+            const convertFilePath = convertFileSrc(dirPath + "plugin/tea-plugin-template/dist/main.js");
 
-            const plugin = await import(dirPath + "/plugin/tea-plugin-template/dist/main.js");
+            try {
+                // const plugin = await import("C:/Users/admin/AppData/Roaming/com.xieisabug.tea/plugin/tea-plugin-template/dist/main.js" as any);
+                const plugin = await import(convertFilePath as any);
 
-            // 检查 plugin.default 是否是一个类（构造函数）
-            if (typeof plugin.default === 'function') {
-                // 如果是类，创建一个实例
-                const instance = new plugin.default();
-                // 调用实例的 onPluginLoad 方法（如果存在）
-                if (typeof instance.onPluginLoad === 'function') {
-                    instance.onPluginLoad();
-                    setPluginNode(instance.renderComponent());
+                // 检查 plugin.default 是否是一个类（构造函数）
+                if (typeof plugin.default === 'function') {
+                    // 如果是类，创建一个实例
+                    const instance = new plugin.default();
+                    // 调用实例的 onPluginLoad 方法（如果存在）
+                    if (typeof instance.onPluginLoad === 'function') {
+                        instance.onPluginLoad();
+                        setPluginNode(instance.renderComponent());
+                    } else {
+                        console.log("onPluginLoad method not found on the instance");
+                    }
+                } else if (typeof plugin.default === 'object') {
+                    // 如果是对象，直接使用
+                    if (typeof plugin.default.onPluginLoad === 'function') {
+                        plugin.default.onPluginLoad();
+                    } else {
+                        console.log("onPluginLoad method not found on the object");
+                    }
                 } else {
-                    console.log("onPluginLoad method not found on the instance");
+                    console.log("Unexpected default export type");
                 }
-            } else if (typeof plugin.default === 'object') {
-                // 如果是对象，直接使用
-                if (typeof plugin.default.onPluginLoad === 'function') {
-                    plugin.default.onPluginLoad();
-                } else {
-                    console.log("onPluginLoad method not found on the object");
-                }
-            } else {
-                console.log("Unexpected default export type");
+            } catch (e) {
+                console.error(e);
             }
+
         }
 
         initPage();
