9dffe99c21cad3244b176ef18daff08922646d09
xieisabug <136419808@qq.com>
2024-10-02 21:20:08 +0800

    Refactor PluginWindow to dynamically load and initialize plugins

--- src/PluginWindow.tsx
+++ src/PluginWindow.tsx
@@ -1,43 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { appDataDir } from '@tauri-apps/api/path';
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 
 function PluginWindow() {
+    window.React = React;
+    window.ReactDOM = ReactDOM;
     const [pluginNode, setPluginNode] = useState<React.ReactNode>(null);
 
     useEffect(() => {
         const initPage = async () => {
             const dirPath = await appDataDir();
-            const convertFilePath = convertFileSrc(dirPath + "plugin/tea-plugin-template/dist/main.js");
+            const convertFilePath = dirPath + "plugin/tea-plugin-template/dist/main.js";
 
-            try {
-                // const plugin = await import("C:/Users/admin/AppData/Roaming/com.xieisabug.tea/plugin/tea-plugin-template/dist/main.js" as any);
-                const plugin = await import(convertFilePath as any);
-
-                // 检查 plugin.default 是否是一个类（构造函数）
-                if (typeof plugin.default === 'function') {
-                    // 如果是类，创建一个实例
-                    const instance = new plugin.default();
-                    // 调用实例的 onPluginLoad 方法（如果存在）
+            // 加载脚本
+            const script = document.createElement('script');
+            script.src = convertFileSrc(convertFilePath);
+            script.onload = () => {
+                // 脚本加载完成后，插件应该可以在全局范围内使用
+                const SamplePlugin = (window as any).SamplePlugin;
+                if (SamplePlugin) {
+                    const instance = new SamplePlugin();
                     if (typeof instance.onPluginLoad === 'function') {
                         instance.onPluginLoad();
                         setPluginNode(instance.renderComponent());
-                    } else {
-                        console.log("onPluginLoad method not found on the instance");
                     }
-                } else if (typeof plugin.default === 'object') {
-                    // 如果是对象，直接使用
-                    if (typeof plugin.default.onPluginLoad === 'function') {
-                        plugin.default.onPluginLoad();
-                    } else {
-                        console.log("onPluginLoad method not found on the object");
-                    }
-                } else {
-                    console.log("Unexpected default export type");
                 }
-            } catch (e) {
-                console.error(e);
-            }
+            };
+            document.body.appendChild(script);
 
         }
 
