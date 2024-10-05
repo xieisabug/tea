ffa2f83a578efe62cc025697ebe87eb95efe88c4
xiejingyang <136419808@qq.com>
2024-10-05 13:39:03 +0800

    ConversationUI展示selected_text

--- src/components/NewChatComponent.tsx
+++ src/components/NewChatComponent.tsx
@@ -7,23 +7,36 @@ interface AssistantListItem {
 }
 
 interface NewChatComponentProps {
+    selectedText: string;
     selectedAssistant: number;
     setSelectedAssistant: (assistantId: number) => void;
     assistants: AssistantListItem[];
 }
 
-const NewChatComponent: React.FC<NewChatComponentProps> = ({selectedAssistant, setSelectedAssistant, assistants}: NewChatComponentProps) => {
-    return <div className="new-chat" data-tauri-drag-region>
-        <div className="new-chat-hint" data-tauri-drag-region>
-            <AskWindowPrepare />
-            <p data-tauri-drag-region>请选择一个对话，或者选择一个助手开始新聊天</p>
+const NewChatComponent: React.FC<NewChatComponentProps> = ({
+    selectedText,
+    selectedAssistant,
+    setSelectedAssistant,
+    assistants,
+}: NewChatComponentProps) => {
+    return (
+        <div className="new-chat" data-tauri-drag-region>
+            <div className="new-chat-hint" data-tauri-drag-region>
+                <AskWindowPrepare selectedText={selectedText} />
+                <p data-tauri-drag-region>
+                    请选择一个对话，或者选择一个助手开始新聊天
+                </p>
+            </div>
+            <CustomSelect
+                options={assistants.map((assistant) => ({
+                    value: assistant.id.toString(),
+                    label: assistant.name,
+                }))}
+                value={selectedAssistant + ""}
+                onChange={(value) => setSelectedAssistant(+value)}
+            />
         </div>
-        <CustomSelect
-            options={assistants.map((assistant) => ({value: assistant.id.toString(), label: assistant.name}))}
-            value={selectedAssistant+""}
-            onChange={(value) => setSelectedAssistant(+value)}
-        />
-    </div>
-}
+    );
+};
 
 export default NewChatComponent;
